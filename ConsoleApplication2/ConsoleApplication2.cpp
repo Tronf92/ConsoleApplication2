162993373cdefbb19655988c434cb24a2210ba8d
adrian <ady.niko51@gmail.com>
2017-03-07 13:28:53 +0200

    study

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,12 @@
+// ConsoleApplication2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+#include <iostream>
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	system("pause");
+	return 0;
+}
+
